25ce5e9ad324d5ce692da8a257971720979988cd
cpjamp95 <cameron.p@windowslive.com>
2026-06-29 16:15:49 +0100

    Initial Astro project setup

---
+++ src/data/wedding.ts
@@ -0,0 +1,25 @@
+export const wedding = {
+  websiteName: "The Bishlingtons",
+
+  couple: {
+    partnerOne: {
+      firstName: "Cameron",
+      surname: "Pilkington",
+    },
+
+    partnerTwo: {
+      firstName: "Alex",
+      surname: "Bishop",
+    },
+  },
+
+  date: {
+    display: "25 June 2027",
+    iso: "2027-06-25",
+  },
+
+  venue: {
+    name: "The Mitre",
+    location: "Hampton Court",
+  },
+} as const;
